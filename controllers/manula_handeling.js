ccace33b984514bd78e6bf6376e0776978d82511
harsh4677 <harshsantoshsalve@gmail.com>
2024-09-06 00:07:57 +0530

    Manual handeling of jwt

---
+++ controllers/manula_handeling.js
@@ -0,0 +1,20 @@
+import jwt from  "jsonwebtoken"
+import errorhandler from "./error/error.js"
+
+const verifyToken = async(req, res, next)=>{
+    const {access_token: token} = req.cookies;
+
+    if(!token){
+        return next(errorhandler(403, "Access denied"))
+    }
+
+    try{
+        const user = await jwt.verify(token, process.env.JWT_SECERT)
+        req.user = user;
+        next()
+    }
+    catch(err){
+        return next(errorHandler(401, "Invalid token"))
+    }
+
+}
